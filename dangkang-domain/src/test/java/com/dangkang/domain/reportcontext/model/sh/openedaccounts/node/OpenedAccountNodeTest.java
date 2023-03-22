83ec06c8a0213a82b16121af86518856602d06d5
zhiliangzhi2 <118045454+zhiliangzhi2@users.noreply.github.com>
2023-03-22 15:02:29 +0800

    重构单元测试为参数化的单元测试

--- dangkang-domain/src/test/java/com/dangkang/domain/reportcontext/model/sh/openedaccounts/node/OpenedAccountNodeTest.java
+++ dangkang-domain/src/test/java/com/dangkang/domain/reportcontext/model/sh/openedaccounts/node/OpenedAccountNodeTest.java
@@ -1,6 +1,10 @@
 package com.dangkang.domain.reportcontext.model.sh.openedaccounts.node;
 
+import com.dangkang.domain.reportcontext.model.sh.openedaccounts.field.EscrowAccountField;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +12,22 @@ class OpenedAccountNodeTest {
 
     @Test
     void format() {
+    }
+    @DisplayName("EscrowAccountField.format方法正确性单元测试")
+   @Test
+    public void shouldGetRightEscrowAccountField(){
+        String expected="93                ";//长度为18
+        assertEquals(expected,new EscrowAccountField("93").format());
+        assertEquals(expected,new EscrowAccountField("93 ").format());
+        assertEquals(expected,new EscrowAccountField(expected).format());
+
+        String blank18Length="                  ";//长度为18
+        assertEquals(blank18Length,new EscrowAccountField("").format());
+
+        String str18Length="123456789012345678";
+        assertEquals(str18Length,new EscrowAccountField(str18Length).format());
+
+        String chinese18Length="中国                ";
+        assertEquals(chinese18Length,new EscrowAccountField("中国").format());
     }
 }
