717870229602912b08539d94ca8e666466fd72c4
zhiliangzhi2 <118045454+zhiliangzhi2@users.noreply.github.com>
2023-03-22 14:29:31 +0800

    重构单元测试为参数化的单元测试

---
+++ dangkang-domain/src/test/java/com/dangkang/domain/reportcontext/model/sh/openedaccounts/node/OpenedAccountNodeTest.java
@@ -0,0 +1,12 @@
+package com.dangkang.domain.reportcontext.model.sh.openedaccounts.node;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OpenedAccountNodeTest {
+
+    @Test
+    void format() {
+    }
+}
